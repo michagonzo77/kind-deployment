345ded06f41f264caab5b18d227417b62fead3fe
Michael Gonzalez <112521273+michagonzo77@users.noreply.github.com>
2024-01-16 22:20:42 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # Install Docker
-sudo apt update
-sudo apt install -y docker.io
+sudo apt-get update
+sudo apt-get install -y docker.io
 
 # Install kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+KUBECTL_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
+curl -LO "https://dl.k8s.io/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
 
-# Install Kind
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
+# Install Kind with the latest version
+KIND_VERSION=$(curl -s https://api.github.com/repos/kubernetes-sigs/kind/releases/latest | grep tag_name | cut -d '"' -f 4)
+curl -Lo ./kind "https://kind.sigs.k8s.io/dl/$KIND_VERSION/kind-linux-amd64"
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 
@@ -23,15 +25,4 @@ sudo chmod 777 $HOME/.kube
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 sudo kind get kubeconfig >> $HOME/.kube/config
 
-# # Install Arkade
-# curl -SLsf https://get.arkade.dev/ | sudo sh
-
-# # Install OpenFaaS app using Arkade
-# arkade install openfaas
-
-# # Get the password from the installed OpenFaaS installation
-# PASSWORD=$(kubectl get secret -n openfaas basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode; echo)
-
-# echo "Gateway password is: ${PASSWORD}
-# Paste this password in the runner creation prompt in Kubiya"
 echo "Done!"
